11c0c3b32eddafa8e58cecd00c15fe8f7843075e
Damini Kesharkar <daminisk272@gmail.com>
2024-01-11 13:38:07 +0530

    Added bcrypt

--- controllers/main.js
+++ controllers/main.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const bcrypt = require('bcrypt');
 const indexFilePath = path.join(__dirname, '../views/index.html');
 
 const Users = require('../models/appointment');
@@ -12,46 +13,70 @@ exports.postUser = (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    Users.findOne({ email: email })
-        .then(existingUser => {
-            if (existingUser) {
-                return res.status(400).json({
-                    message: 'User already exists with this email address'
-                });
-            }
-
-            return Users.create({
-                username: username,
-                email: email,
-                password: password
-            });
-        })
-        .then(createdUser => {
-            if (createdUser) {
-                return res.status(201).json({
-                    message: 'User added successfully',
-                    user: createdUser
-                });
-            }
-        })
-        .catch(err => {
+    bcrypt.hash(password, 10, (err, hashedPassword) => {
+        if (err) {
             console.log(err);
-            res.status(500).json({
+            return res.status(500).json({
                 error: 'Internal Server Error'
             });
-        });
+        }
+
+        Users.findOne({ email: email })
+            .then(existingUser => {
+                if (existingUser) {
+                    return res.status(400).json({
+                        message: 'User already exists with this email address'
+                    });
+                }
+
+                return Users.create({
+                    username: username,
+                    email: email,
+                    password: hashedPassword 
+                });
+            })
+            .then(createdUser => {
+                if (createdUser) {
+                    return res.status(201).json({
+                        message: 'User added successfully',
+                        user: createdUser
+                    });
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    error: 'Internal Server Error'
+                });
+            });
+    });
 };
 
-exports.login = (req,res)=>{
+exports.login = (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    Users.findOne({ email: email, password: password })
+    Users.findOne({ email: email })
         .then(user => {
             if (user) {
-                res.status(200).json({
-                    message: 'User login successful',
-                    user: user
+                bcrypt.compare(password, user.password, (err, passwordMatch) => {
+                    if (err) {
+                        console.log(err);
+                        return res.status(500).json({
+                            error: 'Internal Server Error'
+                        });
+                    }
+
+                    if (passwordMatch) {
+                        res.status(200).json({
+                            message: 'User login successful',
+                            user: user
+                        });
+                    } else {
+                        res.status(401).json({
+                            message: 'User not authorized'
+                        });
+                    }
                 });
             } else {
                 res.status(401).json({
@@ -65,4 +90,4 @@ exports.login = (req,res)=>{
                 error: 'Internal Server Error'
             });
         });
-}
+};
